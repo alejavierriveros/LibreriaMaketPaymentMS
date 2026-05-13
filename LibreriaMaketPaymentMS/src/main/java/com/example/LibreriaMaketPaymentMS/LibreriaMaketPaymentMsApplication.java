2c9c715203ce2bf6934bb28697fa197f8ad88d3c
alejavierriveros <129995554+alejavierriveros@users.noreply.github.com>
2026-05-13 15:12:15 -0400

    main
    subiendo el proyecto por primera ves

---
+++ LibreriaMaketPaymentMS/src/main/java/com/example/LibreriaMaketPaymentMS/LibreriaMaketPaymentMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibreriaMaketPaymentMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaMaketPaymentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaMaketPaymentMsApplication.class, args);
+	}
+
+}
---
+++ LibreriaMaketPaymentMS/src/test/java/com/example/LibreriaMaketPaymentMS/LibreriaMaketPaymentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibreriaMaketPaymentMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaMaketPaymentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

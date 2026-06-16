07b3a313f94f502e0b3dac086d3b62cdd3537814
alejavierriveros <alej.riveros@duocuc.cl>
2026-06-16 12:03:55 -0400

    v3.1
    se me olivido guardar esta parte donde se importan las extenciones de hateoas en PagoModelAssemblers

--- LibreriaMaketPaymentMS/src/main/java/com/example/LibreriaMaketPaymentMS/assemblers/PagoModelAssemblers.java
+++ LibreriaMaketPaymentMS/src/main/java/com/example/LibreriaMaketPaymentMS/assemblers/PagoModelAssemblers.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Component;
 import com.example.LibreriaMaketPaymentMS.controller.PagoRESTControllerV2;
 import com.example.LibreriaMaketPaymentMS.dto.PagoResponseDTO;
 
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
 public class PagoModelAssemblers implements RepresentationModelAssembler<PagoResponseDTO, EntityModel<PagoResponseDTO>> {
 
     @Override
     public EntityModel<PagoResponseDTO> toModel(PagoResponseDTO pago) {
         return EntityModel.of(pago,
-                linkTo(methodOn(PagoRESTControllerV2.class).getById(pago.getId())).withSelfRel(),
-                linkTo(methodOn(PagoRESTControllerV2.class).getAll()).withRel("pagos")
-            );
+                linkTo(methodOn(PagoRESTControllerV2.class).findById(pago.getId())).withSelfRel(),
+                linkTo(methodOn(PagoRESTControllerV2.class).findAll()).withRel("pagos")
+        );
     }
-
 }
